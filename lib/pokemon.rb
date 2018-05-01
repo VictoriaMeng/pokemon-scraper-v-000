9b8086417d0e3608c5546fd7dd09afd7ad2a9388
Victoria Meng <nights1ststar@protonmail.com>
2018-05-01 22:44:22 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,12 +3,12 @@ class Pokemon
 
   @@all = []
 
-  def initialize(id:, type:, name:, db:, hp: 60)
+  def initialize(id:, type:, name:, db:)
     @id = id
     @name = name
     @type = type
     @db = db
-    @hp = hp
+    @hp = 60
     @@all << self
   end
 
